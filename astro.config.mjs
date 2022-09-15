96d9bbe144c3ca8436a94271fb05b028097315a5
VidarDev <vidar@24x7.gg>
2022-09-15 13:17:53 +0200

    🚧 Create Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
+  }
 });
